398d1aee53956f7904922ca5ebc2bd086788415c
msamu001 <msamu001@gold.ac.uk>
2018-09-23 21:06:13 +0100

    change Decoder name
    AI is a more suitable name for the class

---
+++ src/AI.java
@@ -0,0 +1,20 @@
+
+public class AI {
+	private int[] code;
+	
+	public AI() {
+		code = genCode();
+	}
+	
+	public void addKey(int black, int white) {
+		
+	}
+	
+	public int[] calcKey(int[] userGuess) {
+		return null;
+	}
+	
+	private int[] genCode() {
+		return null;
+	}
+}
